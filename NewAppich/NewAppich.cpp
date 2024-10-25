60c220c58552aac02929dd649d05a45833d96373
King <sigma.mindest@gmail.com>
2024-10-25 15:15:15 +0500

    Добавил Accerman

--- NewAppich/NewAppich.cpp
+++ NewAppich/NewAppich.cpp
@@ -12,6 +12,13 @@ int fib(int n) {
     return fib(n - 1) + fib(n - 2);
 }
 
+int ackermann(int m, int n) {
+    if (m == 0) return n + 1;
+    if (m == 1) return n + 2;
+    if (m == 2) return 2 * n + 3;
+    if (m == 3) return (1 << (n + 3)) - 3;
+    return ackermann(m - 1, ackermann(m, n - 1));
+}
 
 int main()
 {
@@ -19,6 +26,11 @@ int main()
 
     
     cout << fib(n);
+
+    
+    int a, b;
+    cin >> a >> b;
+    cout << ackermann(a, b) << endl;
 }
 
 
